241f6f318ab276c065825ea3b129cae9724f0b75
Enoch Boyetey <etmboyetey@gmail.com>
2023-03-02 16:07:06 -0800

    advanced tasks correction

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,26 +1,25 @@
 #include "main.h"
 
 /**
- * print_number - print numbers chars
- * @n: integer params
- * Return: 0
+ * print_number - return the same number as inputted
+ *
+ * @n: return same number as inputted
+ * Return: same number as entered
  */
 
 void print_number(int n)
 {
-	unsigned int n1;
+	unsigned int pnb;
 
-	n1 = n;
-
+	pnb = n;
 	if (n < 0)
 	{
-		_putchar('-');
-		n1 = -n;
+		_putchar(45);
+		pnb = -n;
 	}
-
-	if (n1 / 10 != 0)
+	if (pnb / 10)
 	{
-		print_number(n1 / 10);
+		print_number(pnb / 10);
 	}
-	_putchar((n1 % 10) + '0');
+		_putchar((pnb % 10) + '0');
 }
